859fa6210234603a000de342547f2e1d461d50b2
Atllas1981 <samuelsanchez.rueda@gmail.com>
2025-07-17 14:05:44 -0500

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Atllas1981.github.io/deployar ',
+  base: '/deployar/',
   vite: {
     plugins: [tailwindcss()]
   }
